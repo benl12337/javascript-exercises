8b8342ee603580ca6f17fb8d0a6c62e4117458ef
ben <luben12337@gmail.com>
2022-09-06 20:10:25 +1000

    Finish problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let buffer = '';
+  let sLength = string.length;
+  for (let i = sLength - 1; i >= 0; i--) {
+    buffer = buffer + string[i];
+  }
+  return buffer;
 };
 
 // Do not edit below this line
