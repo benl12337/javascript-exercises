1327af67a3f191bd0a61e4c41470111437d9db87
ben <luben12337@gmail.com>
2022-09-06 19:47:12 +1000

    Complete repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(text, number) {
 
+    if (number == 0) {
+        return '';
+    } else if (number < 0) {
+        return 'ERROR';
+    }
+
+
+    let i = 1;
+    let string = text;
+    while (i < number) {
+        string += text;
+        i++;
+    }
+    return string;
 };
 
 // Do not edit below this line
