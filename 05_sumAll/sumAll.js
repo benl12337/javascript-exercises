39e8e854c1e0a1410fd014717995aea6948b2830
ben <luben12337@gmail.com>
2022-09-11 13:39:20 +1000

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  let sum = 0;
+
+  if (min < 0 || max < 0) {
+    return 'ERROR';
+  }
+
+  if (typeof(max) != 'number' || typeof(min) != 'number') {
+    return 'ERROR';
+  }
+
+  if (max < min) {
+    let tmp = min;
+    min = max;
+    max = tmp;
+  }
+
+  for (let i = min; i <= max; i++) {
+  sum += i;
+  }
+
+  return sum;
 
 };
 
