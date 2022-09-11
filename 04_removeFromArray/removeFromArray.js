5b2f4e06eeb4a94d184685fdb5ca35e818f49de2
ben <luben12337@gmail.com>
2022-09-11 20:04:05 +1000

    Complete the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
 const removeFromArray = function() {
+  inputArray = arguments[0];
+  let arrayLength = inputArray.length;
 
+  removeArray = [];
+  for (let i = 1; i < arguments.length; i++) {
+    removeArray.push(arguments[i]);
+  }
+  
+
+  newArray = [];
+  for (let j = 0; j < arrayLength; j++) {
+    if (removeArray.includes(inputArray[j])) {
+      continue;
+    } else {
+      newArray.push(inputArray[j]);
+    }
+  }
+
+
+  return newArray;
 };
 
 // Do not edit below this line
